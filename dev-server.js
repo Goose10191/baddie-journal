adee975d1d4009095559264a72b72ea8fdfcafb0
Eric Bertram <bertramet@omu.org>
2026-07-30 11:19:11 -0500

    Logo watermark background with frosted-glass translucent surfaces; fix dev-server .css MIME type; sw cache v4

--- dev-server.js
+++ dev-server.js
@@ -8,6 +8,7 @@ const PORT = 8585;
 const ROOT = __dirname;
 const TYPES = {
   '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
   '.js': 'text/javascript; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
   '.webmanifest': 'application/manifest+json; charset=utf-8',
